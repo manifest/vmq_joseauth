8f16e5bd04d7070e31c0945fd2c2d22c9c233b0e
Andrei Nesterov <ae.nesterov@gmail.com>
2016-09-19 14:55:21 +0300

    Unify the Docker run script

--- run-docker.sh
+++ run-docker.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-VMQ_JOSEAUTH_DIR='/opt/manifest/vmq_joseauth'
+PROJECT_DIR='/opt/manifest/vmq_joseauth'
 
 read -r DOCKER_RUN_COMMAND <<-EOF
 	vernemq start \
@@ -10,8 +10,8 @@ EOF
 
 docker build -t manifest/vmq_joseauth .
 docker run -ti --rm \
-	-v $(pwd):${VMQ_JOSEAUTH_DIR} \
+	-v $(pwd):${PROJECT_DIR} \
 	-p 1883:1883 \
 	-p 8888:8888 \
 	manifest/vmq_joseauth \
-	/bin/bash -c "set -x && ${DOCKER_RUN_COMMAND} && set +x && cd ${VMQ_JOSEAUTH_DIR} && /bin/bash"
+	/bin/bash -c "set -x && ${DOCKER_RUN_COMMAND} && set +x && cd ${PROJECT_DIR} && /bin/bash"
